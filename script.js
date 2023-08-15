26904366c8e99006a41211690cbe5617f5ad66c5
ChimdinduDivine <Chimdi569@gmail.com>
2023-08-15 16:51:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// function addTwoNumbers (number1 , number2) {
+
+
+// return number1 + number2;
+// }
+
+// let result=addTwoNumbers(6,10)
+// console.log(result);
+
+
+//CREAT A FUNCTION THAT ADDS 2 NUMBERS AND IS DIVIDED BY 3RD PARAMETER
+// function addAndDivideThreeNumbers (number1 , number2 , number3) {
+//     return (number1 + number2 )/ number3;
+//     }
+    
+//     let result=addAndDivideThreeNumbers(8,10,2)
+//     console.log(result);
+
+//     //ADDING A FUNCTION THAT IS USED TO GREET
+//     function greetMe(name,timeOfTheDay) {
+//         if (timeOfTheDay ==="morning"){
+//         return (`Good morning ${name}`);}
+
+//         if (timeOfTheDay ==="afternoon"){
+//         return (`Good night ${name}`);}
+
+//         if (timeOfTheDay ==="night"){
+//         return (`Good night ${name}`);}
+//         }
+
+//         let name="Chimdindu"
+//         let timeOfTheDay="night"
+//     let greeting = greetMe(name,timeOfTheDay);
+//     console.log(greeting)
+
+//     //Object Methods
+//     const Person={
+//         name:"Alice",
+//         age:30
+    // }
+    //ARROW FUNCTION
+// let array=[1,2,3,4,5,6,7,8,9,10]
+
+// function addThreeNumbers (number1 , number2 , number3) {
+//     return number1 + number2 + number3;
+// }
+//     const addNumbers= (num1,num2,num3)=> {
+//         return num1 +num2 + num3
+//     }
+
+
+    let numbers=[1,2,3,4,5,6,7,8,9,10]
+let sum=0;
+    
+    for(let i=0; i < numbers.length;i++ ) {
+            sum=sum + numbers[i] };
+            {
+            console.log(sum)}
+    
+
+    // function addTenNumbers (number1 , number2 , number3,number4,number5,number6,number7,number8,number9,number10) {
+    //     return number1 + number2 + number3 + number4 + number5 + number6 + number7 +number8 +number9 +number10;}
+
+    //     let amount=addTenNumbers(1,2,3,4,5,6,7,8,9,10)
+    //     console.log(amount)
+
+
+
+
+
+    // let Array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    // function showAllEvenNumbers (num2 , num4 , num6 ,num8 ,num10 ,num12 ,num14 ,num16 ,num18 ,num20);
+
+    // console.log
