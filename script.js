ee1b455807f10fe216be30f71a5893df4ff42b42
ChimdinduDivine <Chimdi569@gmail.com>
2023-08-16 13:49:26 +0100

    assignmnet on loops and arrays

--- script.js
+++ script.js
@@ -49,15 +49,41 @@
 //     }
 
 
-    let numbers=[1,2,3,4,5,6,7,8,9,10]
-let sum=0;
+//     let numbers=[1,2,3,4,5,6,7,8,9,10]
+// let sum=0;
     
-    for(let i=0; i < numbers.length;i++ ) {
-            sum=sum + numbers[i] };
-            {
-            console.log(sum)}
+//     for(let i=0; i < numbers.length;i++ ) {
+//             sum=sum + numbers[i] };
+//             {
+//             console.log(sum)}
     
 
+            //ANOTHER WAY 
+            let array=[1,2,3,4,5,6,7,8,9,10]
+            function totalNumbersInAnArray () {
+                let sum=0;
+    
+                for(let i=0; i < array.length;i++ ) {
+                        sum=sum + array[i] };
+                    
+                        return sum;}
+
+const totalSumOfArray= totalNumbersInAnArray(array);
+console.log(totalSumOfArray);
+
+let num=[1,2,3,4,5]
+            function MultiplicationNumbersInAnArray () {
+                let multiply=1;
+    
+                for(let i=0; i < num.length;i++ ) {
+                        multiply=multiply * num[i] };
+                    
+                        return multiply;};
+
+const totalMultiplicationOfArray= MultiplicationNumbersInAnArray(num);
+console.log(totalMultiplicationOfArray);
+
+            
     // function addTenNumbers (number1 , number2 , number3,number4,number5,number6,number7,number8,number9,number10) {
     //     return number1 + number2 + number3 + number4 + number5 + number6 + number7 +number8 +number9 +number10;}
 
